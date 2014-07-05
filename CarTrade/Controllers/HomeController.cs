6ba5ff392ed19bd548ec54618a96952887eafe01
Völgyi Ádám <volgyiadam@gmail.com>
2014-07-05 10:51:52 +0200

    2

--- CarTrade/Controllers/HomeController.cs
+++ CarTrade/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace CarTrade.Controllers
     {
         public ActionResult Index()
         {
-            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
         }
