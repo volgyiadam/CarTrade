65c89245fb251d0488982edfb010a384c41b9b24
Völgyi Ádám <volgyiadam@gmail.com>
2014-07-05 11:08:53 +0200

    4

---
+++ CarTrade/Models/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarTrade.Models
+{
+    public class Auto
+    {
+        public int id { get; set; }
+        public string marka { get; set; }
+        public string tipus { get; set; }
+        public int evjarat { get; set; }
+        public DateTime gyartasiIdo { get; set; }
+        public string allapot { get; set; }
+        public int tulajdonosokSzama { get; set; }
+        public int telephelyId { get; set; }
+    }
+}
---
+++ CarTrade/Models/Telephely.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarTrade.Models
+{
+    public class Telephely
+    {
+        public int id { get; set; }
+        public int irSzam { get; set; }
+        public string cim { get; set; }
+        public int parkolohely { get; set; }
+    }
+}
